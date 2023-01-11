dd7585e5034b180e5d78df159836b92066ca843b
Sergo <sergojadesu@gmail.com>
2023-01-11 11:50:09 +1000

    home work completed

--- app.py
+++ app.py
@@ -36,6 +36,67 @@ class Genre(db.Model):
     name = db.Column(db.String(255))
 
 
+class MovieSchema(Schema):
+    id = fields.Int()
+    title = fields.Str()
+    description = fields.Str()
+    trailer = fields.Str()
+    year = fields.Int()
+    rating = fields.Float()
+    genre_id = fields.Int()
+    director_id = fields.Int()
+
+
+# class DirectorSchema(Schema):
+#     id = fields.Int()
+#     name = fields.Str()
+#
+#
+# class GenreSchema(Schema):
+#     id = fields.Int()
+#     name = fields.Str()
+
+
+movie_schema = MovieSchema()
+movies_schema = MovieSchema(many=True)
+
+# director_schema = DirectorSchema()
+# directors_schema = DirectorSchema(many=True)
+#
+# genre_schema = GenreSchema()
+# genres_schema = GenreSchema(many=True)
+
+api = Api(app)
+movie_ns = api.namespace('movies')
+# director_ns = api.namespace('directors')
+# genre_ns = api.namespace('genres')
+
+
+@movie_ns.route('/')
+class MovieView(Resource):
+    def get(self):
+        director_id = request.args.get('director_id')
+        genre_id = request.args.get('genre_id')
+        if director_id is not None:
+            all_movies = db.session.query(Movie).filter(Movie.director_id == director_id).all()
+            return movies_schema.dump(all_movies), 200
+        elif genre_id is not None:
+            all_movies = db.session.query(Movie).filter(Movie.genre_id == genre_id).all()
+            return movies_schema.dump(all_movies), 200
+        else:
+            all_movies = db.session.query(Movie).all()
+            return movies_schema.dump(all_movies), 200
+
+
+@movie_ns.route('/<int:uid>')
+class MovieView(Resource):
+    def get(self, uid: int):
+        try:
+            movie = db.session.query(Movie).filter(Movie.id == uid).one()
+            return movie_schema.dump(movie), 200
+        except Exception:
+            return "Not found", 404
+
 
 if __name__ == '__main__':
     app.run(debug=True)
